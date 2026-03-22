3df54d1beb4f1912aa2ff38bf178c70b5c571397
Краївський Станіслав <145298992+farp1n@users.noreply.github.com>
2026-03-23 01:07:04 +0200

    Update script.js

--- Lab/script.js
+++ Lab/script.js
@@ -1,1 +1,21 @@
+(function () {
+  var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
 
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+
+  console.log("--- Selection by the last letter ---");
+  for (var i = 0; i < names.length; i++) {
+    var lastLetter = names[i].charAt(names[i].length - 1).toLowerCase();
+    if (lastLetter === 'a' || lastLetter === 'n') {
+      console.log("Special selection (ends with a/n): " + names[i]);
+    }
+  }
+})();
